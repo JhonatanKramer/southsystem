f0f60c72c5ff4e8c4a87cf4359ee93ebd020dad1
jhonatan <jckramer@universo.univates.br>
2021-04-17 09:17:16 -0300

    iniciando instalado Spring

---
+++ src/test/java/com/southsystem/southsystem/SouthsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.southsystem.southsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SouthsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
